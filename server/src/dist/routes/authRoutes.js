6cacdc247002313daf3ed08fb274886c731e1ddd
jordanss1 <101638406+jordanss1@users.noreply.github.com>
2023-09-10 18:31:26 +0100

    transpiled TS

--- server/src/dist/routes/authRoutes.js
+++ server/src/dist/routes/authRoutes.js
@@ -7,10 +7,12 @@ exports.googleAuthRoutes = void 0;
 const passport_1 = __importDefault(require("passport"));
 const googleAuthRoutes = (app) => {
     app.get("/auth/google", passport_1.default.authenticate("google", { scope: ["profile", "email"] }));
-    app.get("/auth/google/callback", passport_1.default.authenticate("google"));
+    app.get("/auth/google/callback", passport_1.default.authenticate("google"), (req, res) => {
+        res.redirect("/surveys");
+    });
     app.get("/api/logout", (req, res) => {
         req.logOut({}, () => { });
-        res.send(req.user);
+        res.redirect("/");
     });
     app.get("/api/current_user", (req, res) => {
         res.send(req.user);
